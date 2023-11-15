f72d7dc212e3d5d7eea8d4f5a12c7e02aa2a8985
George Arbanas <george.arbanas@icloud.com>
2023-11-15 19:40:49 +0900

    added manifest.json

--- clip.js
+++ clip.js
@@ -9,9 +9,10 @@ async function automatePageSource(address) {
 
   // Get the page source
   const pageSource = await page.content();
-
+  getContent(pageSource);
   // Save the page source to a file (e.g., output.html)
   const fs = require("fs");
+  // get filter out the content you want, and save that content as an HTML file
   fs.writeFileSync("output.html", pageSource);
 
   await browser.close();
