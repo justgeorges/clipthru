dac01188c725a3ab8bdaa94eeec1a5f57b54dcd7
George Arbanas <george.arbanas@icloud.com>
2023-11-15 19:49:29 +0900

    added basic functionality

--- clip.js
+++ clip.js
@@ -1,23 +1,7 @@
-const puppeteer = require("puppeteer");
-
-async function automatePageSource(address) {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-
-  // Navigate to the specified address
-  await page.goto(address);
-
-  // Get the page source
-  const pageSource = await page.content();
-  getContent(pageSource);
-  // Save the page source to a file (e.g., output.html)
-  const fs = require("fs");
-  // get filter out the content you want, and save that content as an HTML file
-  fs.writeFileSync("output.html", pageSource);
-
-  await browser.close();
-}
-
-// Example usage
-const address = "https://google.com";
-automatePageSource(address);
+document.getElementById("clipButton").addEventListener("click", function () {
+  // Implement web clipping logic here
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { action: "clip" });
+  });
+});
